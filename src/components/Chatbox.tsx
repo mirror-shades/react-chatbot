cc6322a569de997bebab5be2aaa40cfffabc5fdf
mirror-shades <44949816+mirror-shades@users.noreply.github.com>
2023-03-21 21:44:05 -0700

    it's alive!
    Chatbot now responds when you talk. You must put your API key into the .env variable to run

--- src/components/Chatbox.tsx
+++ src/components/Chatbox.tsx
@@ -1,29 +1,43 @@
 import React, { useState } from "react";
 import "../App.css";
+import { Configuration, OpenAIApi } from "openai";
 
+const configuration = new Configuration({
+  apiKey: import.meta.env.VITE_OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+var index = 0;
 const Chatbox: React.FC = () => {
   const [messages, setMessages] = useState<string[]>([]);
   const [inputValue, setInputValue] = useState<string>("");
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (inputValue === "") return;
+
     setMessages([...messages, inputValue]);
     setInputValue("");
-    const response = fetch("http://localhost:3000", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ message: inputValue }),
+    const response = await openai.createCompletion({
+      model: "text-davinci-003",
+      prompt: inputValue,
+      temperature: 0.9,
+      max_tokens: 150,
+      top_p: 1,
+      frequency_penalty: 0.0,
+      presence_penalty: 0.6,
+      stop: [" Human:", " AI:"],
     });
-    console.log(response);
+    console.log(response.data.choices[0].text);
+    setMessages((messages) => [
+      ...messages,
+      response.data.choices[0].text ?? "Something went wrong",
+    ]);
   };
 
   return (
     <div>
       <ul>
         {messages.map((message) => (
-          <li>{message}</li>
+          <li key={index++}>{message}</li>
         ))}
       </ul>
       <input
@@ -31,6 +45,7 @@ const Chatbox: React.FC = () => {
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
+      <p />
       <button onClick={handleSendMessage}>Send</button>
     </div>
   );
